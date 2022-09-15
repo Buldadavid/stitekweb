025e82121ab582f9a096571eb5a19a4e75c68782
Buldadavid <104922435+Buldadavid@users.noreply.github.com>
2022-09-15 13:21:18 +0200

    Add files via upload

---
+++ WebAPP/WebAPP.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request,redirect
+import csv
+from openpyxl import load_workbook
+import stitek
+from datetime import date
+import os
+
+
+app = Flask(__name__)
+header = ['QR','MLFB','cislo','z','brzda','odmer','vaha','tep','dat_v','barvaO','barvaZ']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+
+@app.route('/', methods=['POST', 'GET'])
+def indexV():
+	row = []	
+	QR = request.form['text']
+	print(QR)
+	
+	brzda = request.form['text2']
+	print(brzda)
+	
+	vaha = request.form['text3']
+	print(vaha)
+	
+	tep = request.form.get('tep')
+	print(tep)
+	
+	if " " in QR :
+		return render_template('error_qr.html',Imput=QR) 
+
+	MLFB,cislo,odmer,Z,dat_v,barvaO,barvaZ = stitek.vyroba(QR)
+	
+	#pridani do listu
+	row.append(QR)
+	row.append(MLFB)
+	row.append(cislo)
+	row.append(Z)
+	row.append(brzda)
+	row.append(odmer)
+	row.append(vaha)
+	row.append(tep)
+	row.append(dat_v)
+	row.append(barvaO)
+	row.append(barvaZ)
+	
+	with open('data/data.csv', 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(header)
+		writer.writerow(row)
+	
+	QR,MLFB,Z,cislo,odmer,dat_v,barva,vibroOK,vibroNOK,barvaO,barvaZ = stitek.stitek()
+	print(barva)
+	return render_template('index.html', z=Z, mlfb=MLFB, datum=dat_v, odmer = odmer, cislo = cislo, Imput= QR,barvaO = barvaO, barvaZ=barvaZ,barva = barva)
+    
+@app.route('/manualne', methods=['GET'])
+def indexM():
+	return render_template('indexM.html')
+
+@app.route('/manualne', methods=['POST'])
+def indexMV():
+	MLFB = request.form['text']
+	print(MLFB)
+	cislo = request.form['text2']
+	print(cislo)
+	Z = request.form['textz']
+	print(Z)
+	brzda = request.form['text3']
+	print(brzda)
+	odmer = request.form['text4']
+	print(odmer)
+	vaha = request.form['text5']
+	print(vaha)        
+	tep = request.form.get('tep')
+	print(tep)
+
+	QR = "?"
+	dat_v = stitek.vyroba2(cislo)
+	row = []
+
+	row.append(QR)
+	row.append(MLFB)
+	row.append(cislo)
+	row.append(Z)
+	row.append(brzda)
+	row.append(odmer)
+	row.append(vaha)
+	row.append(tep)
+	row.append(dat_v)
+	print(MLFB)
+
+	with open('data/data.csv', 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(header)
+		writer.writerow(row)
+    
+	return redirect('/uka')
+
+@app.route('/uka', methods=['POST', 'GET'])
+def uka():
+	QR,MLFB,Z,cislo,odmer,dat_v,barva,vibroOK,vibroNOK,barvaO,barvaZ  = stitek.stitek()
+	return render_template('stitek.html',vibroOK = vibroOK, vibroNOK = vibroNOK , z=Z, mlfb=MLFB, datum=dat_v, odmer = odmer, cislo = cislo, barvaO = barvaO, barvaZ=barvaZ, barva = barva, Imput = QR)
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    return redirect('/')
+
+@app.route("/tisk", methods=['POST'])
+def tisker():
+    print("tisk")
+    #os.system("lp -o media=1*1.9 /static/pillow_paste.jpg")
+    return redirect("/uka")
+
+app.run(host="0.0.0.0")
---
+++ WebAPP/stitek.py
@@ -0,0 +1,184 @@
+from PIL import Image, ImageFont, ImageDraw
+from openpyxl import load_workbook
+import qrcode
+import csv
+import datetime
+from datetime import date
+
+def stitek():
+    data = []
+
+    with open("data/data.csv") as file:
+        reader = csv.DictReader(file)
+
+        for r in reader:
+            data.append({"QR": r["QR"],"MLFB": r["MLFB"], "cislo": r["cislo"],"z": r["z"], "brzda": r["brzda"], "odmer": r["odmer"], "vaha": r["vaha"], "dat_v": r["dat_v"], "tep": r["tep"], "barvaO": r["barvaO"], "barvaZ": r["barvaZ"]})
+
+    #print((data[0])["MLFB"])
+    QR = (data[0])["QR"]
+    MLFB = (data[0])["MLFB"]
+    cislo = (data[0])["cislo"]
+    Z = (data[0])["z"]
+    dat_v = (data[0])["dat_v"]
+    odmer = (data[0])["odmer"]
+    brzda = (data[0])["brzda"]
+    vaha = (data[0])["vaha"]
+    tep = (data[0])["tep"]
+    barvaO = (data[0])["barvaO"]
+    barvaZ = (data[0])["barvaZ"]
+
+    current_time = datetime.datetime.now()
+    dat = str(current_time.year) + "." + str(current_time.month) + "." + str(current_time.day)
+    vibroOK = dat + "_" + MLFB + "_" + cislo + "_16,67Hz_OK_VIB12"
+    vibroNOK = dat + "_" + MLFB + "_" + cislo + "_16,67Hz_NOK_VIB12"
+
+    todays_date = date.today()
+    year = todays_date.year
+    q= year-int(dat_v[-4:])
+    barva = "#fefefe"
+
+    if q > 2 :
+        barva = "#ff0000"
+    elif q == 1:
+        barva = "yellow"
+    elif q == 0:
+        barva = "#74ec3a"
+
+    qr_text = "1P"+MLFB+"+SYF"+cislo.replace("-","")
+
+    input_data = qr_text
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=2,
+        border=1)
+    qr.add_data(input_data)
+    qr.make(fit=True)
+    im2 = qr.make_image(fill='black', back_color='white')
+    my_image = Image.open("tisk-stitku/st.jpg")
+    title_font = ImageFont.truetype("tisk-stitku/Roboto-Medium.ttf", 20)
+
+    image_editable = ImageDraw.Draw(my_image)
+    image_editable.text((25,35), "3 ~ Motor " + MLFB, (0, 0, 0), font=title_font)
+    image_editable.text((25,73), "No. YF " + cislo, (0, 0, 0), font=title_font)
+    image_editable.text((25,105), "Enc. " + odmer, (0, 0, 0), font=title_font)
+    image_editable.text((25,135), "Brake "+ brzda, (0, 0, 0), font=title_font)
+    image_editable.text((25,175), "m "+ vaha +"Kg", (0, 0, 0), font=title_font)
+    image_editable.text((225,105), "Tepl. čidlo " + tep, (0, 0, 0), font=title_font)
+
+    my_image.paste(im2, (340, 135))
+    my_image.save("static/pillow_paste.jpg", quality=95)
+    print("OK")
+    return(QR,MLFB,Z,cislo,odmer,dat_v,barva,vibroOK,vibroNOK,barvaO,barvaZ)
+
+def vyroba(QR):
+    wb = load_workbook(filename = 'data/data.xlsx')
+    ws = wb['nic']
+    
+    imput = QR[2:50]
+    cd = len(imput)
+    #print(cd)
+    
+#    if " " in QR :
+#        return render_template('error/error_qr.html',QR=QR)        
+
+    if cd >= 37 :
+        MLFB = imput[0:20]
+        Z = "?"
+        barvaZ = "#ff0000"
+        #print(z)
+        #print(MLFB)
+        datum = imput[24:26]
+        #print(datum)
+    
+        vc = imput[24:38]
+        wc = len(vc)
+        #print(wc)
+
+
+        if wc == 13 :
+
+            cislo = imput[24:28]+"-"+imput[28:32]+"-"+imput[32:34]+"-"+imput[34:37]
+            #print(cislo)
+
+        elif wc == 14:     
+
+            cislo = imput[24:29]+"-"+imput[29:33]+"-"+imput[33:35]+"-"+imput[35:38]
+            #print(cislo)
+    
+    elif cd < 37 :
+        MLFB = imput[0:18]
+        Z = "/"
+        barvaZ = "#dddddd"
+        #print(MLFB)
+        #print(z)
+        datum = imput[22:24]
+        #print(datum)
+        
+        vc = imput[22:38]
+        wc = len(vc)
+        #print(wc)
+        if wc == 13 :
+
+            cislo = imput[22:26]+"-"+imput[26:30]+"-"+imput[30:32]+"-"+imput[32:35]
+            #print(cislo)
+
+        elif wc == 14:     
+
+            cislo = imput[22:27]+"-"+imput[27:31]+"-"+imput[31:33]+"-"+imput[33:36]
+            #print(cislo)
+        
+
+    #tvorba indexu 1ft6108a-8
+    index = imput[0:6]+imput[15]+imput[7:9]
+    print(index)
+
+    #vyhledání indexu v tabulce
+    for r in ws.rows:
+        if r[0].value == index:
+            odmer = r[1].value
+            barvaO = "#dddddd"
+
+    try:
+        odmer
+    except NameError:
+        #print("Variable is not defined....!")
+        odmer = "?"
+        barvaO = "#ff0000"
+        print(odmer)
+    else:
+        #print("Variable is defined.")
+        print(odmer)
+    
+    #vyhledání data v tabulce
+    for r in ws.rows:
+        if wc == 13 :
+            if r[2].value == datum:
+                dat_v = r[3].value
+                #print(dat_v)
+
+        if wc == 14 :
+            if r[4].value == datum:
+                dat_v = r[5].value
+                #print(dat_v)
+    return(MLFB,cislo,odmer,Z,dat_v,barvaO,barvaZ)	
+
+def vyroba2(cislo):
+    wb = load_workbook(filename = 'data/data.xlsx')
+    ws = wb['nic']
+    cislo = cislo.replace("-","")
+    cislo = cislo.replace(" ","")
+    wc = len(cislo)
+    datum = cislo[0:2]
+
+    #vyhledání data v tabulce
+    for r in ws.rows:
+        if wc == 13 :
+            if r[2].value == datum:
+                dat_v = r[3].value
+                #print(dat_v)
+
+        if wc == 14 :
+            if r[4].value == datum:
+                dat_v = r[5].value
+                #print(dat_v)
+    return(dat_v)	
